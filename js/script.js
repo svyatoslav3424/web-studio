9c4cebbbd599bd4e17a713c422db50516eb26286
svyatoslav3424 <zaitsev.svyatoslav@gmail.com>
2024-12-05 10:07:40 +0200

    burger menu

--- js/script.js
+++ js/script.js
@@ -1,6 +1,18 @@
 const queryBtn = document.querySelector('.hero-btn')
 const closeBtn = document.querySelector('.close-btn')
     
+const menuBurger = document.querySelector('.menu-burger-btn')
+const menuBurgerClose = document.querySelector('.menu-close-btn')
+
+
+menuBurger.addEventListener('click', () => {
+    document.body.classList.add('is-menu-open')
+})
+
+menuBurgerClose.addEventListener('click', () => {
+    document.body.classList.remove('is-menu-open')
+})
+
 queryBtn.addEventListener('click', () => {
     document.body.classList.add('is-modal-open')
 })
